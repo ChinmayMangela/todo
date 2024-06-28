15fd60b535814ccc0fa430170a91a48d0b86a53b
ChinmayMangela <mangelachinmay7@gmail.com>
2024-06-28 18:16:53 +0530

    added functionality to github icon for redirect users to my GitHub account on click.

--- lib/presentation/widgets/app_bar.dart
+++ lib/presentation/widgets/app_bar.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class MyAppBar extends StatelessWidget implements PreferredSizeWidget{
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MyAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
     return AppBar(
       title: const Text('Your Task\'s'),
       actions: [
         Padding(
           padding: const EdgeInsets.all(8.0),
-          child: GestureDetector(
-            onTap: () {},
-            child: Image.asset(
-              'assets/images/github_logo.png',
-              height: 30,
-              width: 30,
+          child: IconButton(
+            onPressed: _navigateToMyGithubAccount,
+            icon: FaIcon(
+              size: 30,
+              FontAwesomeIcons.github,
+              color: isDarkMode ? Colors.white : Colors.black,
             ),
           ),
         ),
       ],
     );
+  }
+
+  void _navigateToMyGithubAccount() async {
+    final url = Uri.parse('https://github.com/ChinmayMangela');
+    if(!await launchUrl(url)) {
+      return;
+    }
   }
 
   @override

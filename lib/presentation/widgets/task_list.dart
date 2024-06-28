fc2872889c65081fa209c04c64478f0ee80e32e9
ChinmayMangela <mangelachinmay7@gmail.com>
2024-06-28 18:47:02 +0530

    Refactor TaskList widget to improve code clarity and organization

---
+++ lib/presentation/widgets/task_list.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/presentation/providers/task_provider.dart';
+import 'package:todo/presentation/widgets/task_tile.dart';
+
+import '../../utils/helper_functions.dart';
+
+class TaskList extends StatelessWidget {
+  const TaskList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    final taskListProvider = Provider.of<TaskProvider>(context);
+    return ListView.builder(
+      itemCount: taskListProvider.tasks.length,
+      itemBuilder: (context, index) {
+        final currentTask = taskListProvider.tasks[index];
+        return Slidable(
+            endActionPane: ActionPane(
+              motion: const StretchMotion(),
+              children: [
+                SlidableAction(
+                  onPressed: (context) {
+                    taskListProvider.removeTask(currentTask);
+                    HelperFunctions.showSnackBar(
+                        context, '${currentTask.name} task removed');
+                  },
+                  backgroundColor:
+                  isDarkMode ? Colors.red.shade900 : Colors.red.shade700,
+                  foregroundColor: isDarkMode ? Colors.black : Colors.white,
+                  icon: Icons.delete,
+                )
+              ],
+            ),
+            child: TaskTile(
+              task: currentTask,
+            ));
+      },
+    );
+  }
+}

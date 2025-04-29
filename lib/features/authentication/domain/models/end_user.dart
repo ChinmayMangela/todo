2d6c30233e9902361bd5f8c33097d08d40233c92
apple <mangelachinmay7@gmail.com>
2025-04-30 00:05:23 +0530

    Implement EndUser model
    This commit introduces the `EndUser` model class, which represents a user with an `id` and `email`. The class includes methods for JSON serialization and deserialization.

---
+++ lib/features/authentication/domain/models/end_user.dart
@@ -0,0 +1,23 @@
+
+
+class EndUser {
+  final String id;
+  final String email;
+
+  const EndUser({required this.id, required this.email});
+
+  factory EndUser.fromJson(Map<String, dynamic> json, String id) {
+    return EndUser(
+      id: id,
+      email: json['email'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'email': email,
+    };
+  }
+
+}
